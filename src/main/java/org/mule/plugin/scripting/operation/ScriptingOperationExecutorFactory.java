5803b7cc57e5661da76fe0de7cc5d0eb1ee097a5
Alejandro García Marra <alemarra@gmail.com>
2017-09-08 00:15:01 +0200

    MULE-11913: Support for router operations in SDK (#34)

--- src/main/java/org/mule/plugin/scripting/operation/ScriptingOperationExecutorFactory.java
+++ src/main/java/org/mule/plugin/scripting/operation/ScriptingOperationExecutorFactory.java
@@ -1,18 +1,18 @@
 package org.mule.plugin.scripting.operation;
 
 import org.mule.runtime.api.meta.model.operation.OperationModel;
-import org.mule.runtime.extension.api.runtime.operation.OperationExecutor;
-import org.mule.runtime.extension.api.runtime.operation.OperationExecutorFactory;
+import org.mule.runtime.extension.api.runtime.operation.ComponentExecutor;
+import org.mule.runtime.extension.api.runtime.operation.ComponentExecutorFactory;
 
 /**
  * Custom factory that creates our operation executor.
  *
  * @since 1.0
  */
-public class ScriptingOperationExecutorFactory implements OperationExecutorFactory {
+public class ScriptingOperationExecutorFactory implements ComponentExecutorFactory<OperationModel> {
 
   @Override
-  public OperationExecutor createExecutor(OperationModel operationModel) {
+  public ComponentExecutor createExecutor(OperationModel operationModel) {
     return new ScriptingOperationExecutor();
   }
 }
