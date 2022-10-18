21f79527fff2c4c5b2b976b3db44f7eadb4362e2
Ignacio Giss <rgiss@mulesoft.com>
2022-10-18 15:07:23 -0300

    W-11619477: add log when scripting engine is defaulted

--- src/main/java/org/mule/plugin/scripting/component/ScriptRunner.java
+++ src/main/java/org/mule/plugin/scripting/component/ScriptRunner.java
@@ -84,7 +84,7 @@ public class ScriptRunner {
 
     if (scriptEngine == null && ECMA_SCRIPT_ENGINE.equalsIgnoreCase(engineName)) {
       scriptEngine = createScriptEngineByName(NASHORN_ENGINE);
-      LOGGER.warn("The Scripting Engine defaulted to " + NASHORN_ENGINE);
+      LOGGER.warn("The " + ECMA_SCRIPT_ENGINE + " Scripting Engine name was not found. The Scripting Engine defaulted to " + NASHORN_ENGINE);
     }
 
     if (scriptEngine == null) {
